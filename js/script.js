0dfd32675e861e2b3e1e8963efa13b744e31f8c3
riz-S <59738393+riz-S@users.noreply.github.com>
2020-05-20 17:31:00 +0800

    fixed all

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-function klik(i){
-    if(document.getElementById(i).className === "drinks-menu"){
-        document.getElementById(i).className = "drinks-menu-active";
-    }
-    else{
-        document.getElementById(i).className = "drinks-menu";
-    }
+function klik(i) {
+  if (document.getElementById(i).className === "drinks-menu") {
+    document.getElementById(i).className = "drinks-menu-active";
+  } else {
+    document.getElementById(i).className = "drinks-menu";
+  }
 }
